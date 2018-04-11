db0f59644e771481f832a0536d44cfca4562f96f
trooper98 <oliver.g.ryan@gmail.com>
2018-04-11 17:15:54 +0200

    building functions

--- src/testing/testing.ino
+++ src/testing/testing.ino
@@ -1,82 +1,100 @@
 #include <Smartcar.h>
+#include <Servo.h>
 
 Car car;
-Gyroscope gyro;
-Sonar sonar1; //front
-const int TRIG_PIN = 5; //sensor's trig pin for back
-const int ECHO_PIN = 4; //sensor's echo pin for front
-Sonar sonar2; //back
-const int TRIG_PIN1 = 7; //sensor's trig pin for back
-const int ECHO_PIN1 = 6; //sensor's echo pin for front
+//---------- gyro ----------
+const int offset = 32; //this was aquired from the gyroscope callibration example
+Gyroscope gyro(offset);
+
+//---------- Sensors ----------
+Sonar front; //front
+const int TRIG_PIN_F = 4; //sensor's trig pin for front
+const int ECHO_PIN_F = 5; //sensor's echo pin for front
+
+Sonar backR; //back rotating
+const int TRIG_PIN_B = 6; //sensor's trig pin for back
+const int ECHO_PIN_B = 7; //sensor's trig pin for back
+
 GP2Y0A21 infra;
-const int frontRightLaser = A0; //use analog pins only
-Odometer encoder(100); //let's say there are exactly 100 pulser per meter
-const int encoderPin = 2;
+const int INFRA_PIN = A0; //use analog pins only
 
-void setup() { 
+//---------- Odometer ----------
+Odometer encoderRight;
+Odometer encoderLeft;
+const int ENCODER_RIGHT_PIN = 3;
+const int ENCODER_LEFT_PIN = 2;
+
+//---------- Tools ----------
+Servo myservo;  // create servo object to control a servo
+int const SERVO_PIN = 13;
+
+//---------- Extra ----------
+const int mainSpeed = .125; // meters per sec
+const int reverseSpeed = .05; // meters per sec
+
+void setup() {
   //attach
-  sonar1.attach(TRIG_PIN, ECHO_PIN);
-  sonar2.attach(TRIG_PIN1, ECHO_PIN1);
-  infra.attach(frontRightLaser);
-  encoder.attach(encoderPin);
+  front.attach(TRIG_PIN_F, ECHO_PIN_F);
+  backR.attach(TRIG_PIN_B, ECHO_PIN_B);
+  infra.attach(INFRA_PIN);
   gyro.attach();
-  
+  myservo.attach(SERVO_PIN);
+  encoderRight.attach(ENCODER_RIGHT_PIN);
+  encoderLeft.attach(ENCODER_LEFT_PIN);
+
   //begin
+  delay(2000);
   Serial.begin(9600);
-  car.begin(gyro);
-  encoder.begin();
-
-
-  //code
-  boolean go = true;
-  int right = infra.getDistance();
-  int frontRightS = sonar1.getDistance();
-  int backS = sonar2.getDistance();
-  int currDistance = encoder.getDistance();
-  car.setMotorSpeed(60,60);
+  gyro.begin(500); //gyro reads every 500ms
+  encoderRight.begin();
+  encoderLeft.begin();
+  car.begin(encoderLeft, encoderRight, gyro);
+  car.enableCruiseControl();
+  car.setSpeed(mainSpeed); //set the speed to x m/sec
+  car.go(1000);
 }
 
 void loop() {
-  // i put code in set up because it doesn't work properly in loop 
-    
-    /*boolean go = true;
-  int right = infra.getDistance();
-  int frontRightSonar = sonar1.getDistance();
-  int backSonar = sonar2.getDistance();
-  int currDistance = encoder.getDistance();
-  
-    while(go){
-    right = infra.getDistance();
-    frontRightSonar = sonar1.getDistance();
-    backSonar = sonar2.getDistance();
-    currDistance = encoder.getDistance();
-      gyro.update();
-        if((right <= 15 && right >0)){
-          go = false;
-        }else{
-           car.setMotorSpeed(50, 50);
-           }
-           delay(400);
-      }*/
-    //Serial.println(gyro.getAngularDisplacement());
-    //Serial.println(frontRightSonar);
-    //Serial.println(backSonar);
-    //Serial.println(right);
+  car.updateMotors(); // for cruise control
+  int rightDistance = encoderRight.getDistance();
+  int leftDistance = encoderLeft.getDistance();
+  int distance = leftDistance;
+  Serial.println(getRearDistance());
+}
 
-  boolean go = true;
-  int right = infra.getDistance();
-  int frontRightSonar = sonar1.getDistance();
-  int backSonar = sonar2.getDistance();
-  int currDistance = encoder.getDistance();
 
-  Serial.println(frontRightSonar);
-  if(frontRightSonar <= 35 && frontRightSonar > 0){ 
-    car.setMotorSpeed(0,0);
-    delay(1000);
-    car.setMotorSpeed(-50, -20);
-    delay(10000);
+//getters
+int getFrontDistance() { //distance of front right sensor
+  return front.getDistance();
+}
+
+int getRearDistance() {
+  return infra.getDistance();
+}
+
+int getRearRightDistance() {
+  return backR.getDistance();
+}
+
+//commands
+void slowDownStop(){
+  for(int i = mainSpeed; i > 0; i--){
+    car.setSpeed(i);
     }
-    
+  }
+
+//tools
+void servoTurn() {
+  int pos = 0;
+  for (pos = 0; pos <= 75; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 5ms for the servo to reach the position
+  }
+  for (pos = 75; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 5ms for the servo to reach the position
+  }
 }
 
 
