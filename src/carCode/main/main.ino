3928a6d5705725ee8cdfd08c66f9f90101272843
NazeehAlhosary <gusalhmn@student.gu.se>
2018-05-16 15:27:08 +0200

    Organize the code a little bit

--- src/carCode/main/main.ino
+++ src/carCode/main/main.ino
@@ -83,8 +83,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   //remoteControl();
-  park();
-  //voiceCommand();
+  //park();
+  voiceCommand();
 
 }
 //---------------------------------------------------------------------------------------------------
